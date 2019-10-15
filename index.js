017882ef14cec69689ced9d41a2389347b95988f
Ranjith <ranju08134@gmail.com>
2019-10-15 18:17:33 +0530

    Updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http=require('http');
 const url=require('url');
 const port=9000;
 const repalceTemplate=require('./modules/replaceTemplate');
+const slugify=require('slugify');
 
 ////////SERVER STARTS HERE
 
@@ -11,6 +12,9 @@ const tempCard=fs.readFileSync(`${__dirname}/src/template-card.html`,'utf-8');
 const tempProduct=fs.readFileSync(`${__dirname}/src/template-product.html`,'utf-8');
 const data=fs.readFileSync(`${__dirname}/json/data.json`,'utf-8');
 const dataObj=JSON.parse(data);
+const slugs=dataObj.map(el=>slugify(el.productName,{lower:true}));
+console.log(slugs);
+// console.log(slugify('Fresh Avocados',{lower:true}));
  
 const server=http.createServer((req,res)=>{
     const {query,pathname}=url.parse(req.url,true);
