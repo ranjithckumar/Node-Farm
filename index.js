39f7bd0e6f733419015a4fe3cc943bf7464c6758
Ranjith <ranju08134@gmail.com>
2019-10-15 16:40:06 +0530

    Added modules file

--- index.js
+++ index.js
@@ -2,20 +2,10 @@ const fs=require('fs');
 const http=require('http');
 const url=require('url');
 const port=9000;
+const repalceTemplate=require('./modules/replaceTemplate');
 
 ////////SERVER STARTS HERE
-const repalceTemplate=(temp,product)=>{
-    let output=temp.replace(/{%PRODUCTNAME%}/g,product.productName);
-    output=output.replace(/{%IMAGE%}/g,product.image);
-    output=output.replace(/{%PRICE%}/g,product.price);
-    output=output.replace(/{%FROM%}/g,product.from);
-    output=output.replace(/{%NUTRIENTS%}/g,product.nutrients);
-    output=output.replace(/{%QUANTITY%}/g,product.quantity);
-    output=output.replace(/{%DESCRIPTION%}/g,product.description);
-    output=output.replace(/{%ID%}/g,product.id);
-    if(!product.organic) output=output.replace(/{%NOT_ORGANIC%}/g,'not_organic');
-    return output;
-}
+
 const tempOverview=fs.readFileSync(`${__dirname}/src/template-overview.html`,'utf-8');
 const tempCard=fs.readFileSync(`${__dirname}/src/template-card.html`,'utf-8');
 const tempProduct=fs.readFileSync(`${__dirname}/src/template-product.html`,'utf-8');
